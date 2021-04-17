df5dfc55f8a468da6c9a5f755a27c14137c3a434
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-04-17 23:35:33 +0300

    Add: EncoderLZW.save_codes() save codes given their bitness

--- encoder.cpp
+++ encoder.cpp
@@ -1,5 +1,8 @@
 #include "encoder.hpp"
 
+// debug
+#include <bitset>
+
 using namespace std;
 
 // debug
@@ -16,10 +19,13 @@ void EncoderLZW::encode(const char *in, const char *out) {
   ifstream input(in, std::ios::in | std::ios::binary);
   ofstream output(out, std::ios::out | std::ios::binary);
 
+  if (!input || !output) {
+    cout << "Cannot open file!" << endl;
+    exit(1);
+  }
+
   cout << "Compressing file " << in;
   queue<int> codes = get_codes(input);
-
-  printQueue(codes);
 
   save_codes(codes, output);
   cout << "Result written to " << out << endl;
@@ -62,16 +68,52 @@ queue<int> EncoderLZW::get_codes(istream &input) {
       cout << ".";
     }
   }
+
   cout << " Done." << endl;
   return output;
 }
 
 // Зберігає чергу кодів LZW у потік output
 void EncoderLZW::save_codes(queue<int> &codes, ostream &output) {
-  char low, high;
+  int output_bit_count = 0, curr, curr_bits, prev = 0, prev_bits = 0, out, offset, mask;
   while (!codes.empty()) {
-    low = codes.front();
-    output.write(&low, 1);
+    curr = codes.front();
+    curr_bits = count_min_bits(curr);
+
+    offset = 8 - output_bit_count % 8;
+
+    if (offset == 8 && prev_bits != 0) {
+      output.write((char *)&prev, 1);
+      prev = prev_bits = 0;
+    }
+
+    out = (curr >> (curr_bits - offset));  // Старші (offset) бітів
+    out |= prev;                           // Об'єднаємо з попередніми бітами
+    output.write((char *)&out, 1);
+
+    cout << curr << ",\t" << out << ",\t" << bitset<8>(out) << ", ";
+
+    prev = 0 | (curr & ((1 << (curr_bits - offset)) - 1));  // Молодші (curr_bits - offset) бітів
+    prev = prev << (8 - curr_bits + offset);
+
+    prev_bits = 8 - offset;
+    output_bit_count += curr_bits;
+
+    cout << prev_bits << ",\t" << bitset<8>(prev) << endl;
+
     codes.pop();
   }
+  if (output_bit_count % 8) {
+    output.write((char *)&prev, 1);
+  }
+}
+
+// Обчислює мінімальну кількість біт для запису коду
+int EncoderLZW::count_min_bits(int code) {
+  for (int i = BITS; i >= 8; i--) {
+    if ((1 << i) & code) {
+      return i + 1;
+    }
+  }
+  return 8;
 }
--- encoder.hpp
+++ encoder.hpp
@@ -11,9 +11,9 @@ class EncoderLZW {
   // кількості біт як MAX_SIZE = 2^BITS
   int BITS, MAX_SIZE;
 
-  int count_min_bits(int n);
   std::queue<int> get_codes(std::istream &input);                 // Повертає дані з потоку input у вигляді черги кодів LZW
   void save_codes(std::queue<int> &codes, std::ostream &output);  // Зберігає чергу кодів LZW у потік output
+  int count_min_bits(int code);                                   // Обчислює мінімальну кількість біт для запису коду
 
  public:
   EncoderLZW(const int bits) : BITS(bits), MAX_SIZE(pow(2, bits)){};
