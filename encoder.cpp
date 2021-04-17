4e2897415d7b4486f38ce8d366a691b2e87b7246
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-04-17 21:04:29 +0300

    Add: EncoderLZW.get_codes() now return queue with LZW codes

--- encoder.cpp
+++ encoder.cpp
@@ -2,13 +2,27 @@
 
 using namespace std;
 
+// debug
+void printQueue(queue<int> q) {
+  while (!q.empty()) {
+    cout << " " << q.front();
+    q.pop();
+  }
+  cout << endl;
+}
+
 // Збергіає зжатий алгоритмом LZW файл in в файл out
 void EncoderLZW::encode(const char *in, const char *out) {
   ifstream input(in, std::ios::in | std::ios::binary);
   ofstream output(out, std::ios::out | std::ios::binary);
 
+  cout << "Compressing file " << in;
   queue<int> codes = get_codes(input);
+
+  printQueue(codes);
+
   save_codes(codes, output);
+  cout << "Result written to " << out << endl;
 
   input.close();
   output.close();
@@ -21,16 +35,34 @@ queue<int> EncoderLZW::get_codes(istream &input) {
 
   // Ініціалізуємо таблицю кодів початковим алфавітом
   map<string, int> dict;
-  for (int i = 0; i < 256; i++)
+  for (int i = 0; i < 256; i++) {
     dict[string(1, i)] = i;
-
-  char c = 0;
-  string w;
-  while (input.read(&c, sizeof(c))) {
-    w = string(1, c);
-    output.push(dict[w]);
   }
+  int dict_size = 256;
 
+  char character, progress_bar_counter;
+  string w, wc;
+  while (input.read(&character, sizeof(character))) {
+    wc = w + character;
+
+    if (dict.count(wc)) {  // Послідовність wс є в таблиці
+      w = wc;
+    } else {  // Послідовності wс немає в таблиці
+
+      output.push(dict[w]);
+      w = string(1, character);
+      // Добавимо нову послідовність в таблицю
+      if (dict_size < MAX_SIZE) {
+        dict[wc] = dict_size++;
+      }
+    }
+
+    // Індикатор прогресу
+    if (++progress_bar_counter % 100 == 0) {
+      cout << ".";
+    }
+  }
+  cout << " Done." << endl;
   return output;
 }
 
--- encoder.hpp
+++ encoder.hpp
@@ -8,14 +8,14 @@ class EncoderLZW {
  private:
   // Максимальна кількість біт в коді, що відповідає закодованим символам.
   // Та максимальний розмір таблиці кодів, що залежить від максимальної
-  // кількості біт як TABLE_SIZE = 2^BITS
-  int BITS, TABLE_SIZE;
+  // кількості біт як MAX_SIZE = 2^BITS
+  int BITS, MAX_SIZE;
 
   int count_min_bits(int n);
   std::queue<int> get_codes(std::istream &input);                 // Повертає дані з потоку input у вигляді черги кодів LZW
   void save_codes(std::queue<int> &codes, std::ostream &output);  // Зберігає чергу кодів LZW у потік output
 
  public:
-  EncoderLZW(const int bits) : BITS(bits), TABLE_SIZE(pow(2, bits)){};
+  EncoderLZW(const int bits) : BITS(bits), MAX_SIZE(pow(2, bits)){};
   void encode(const char *in, const char *out);  // Збергіає зжатий алгоритмом LZW файл in в файл out
 };
