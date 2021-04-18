ed131cbf85a75dc3fcecbc2ebab23533f68198ab
Dmytro Herashchenko <51529317+dmytro-duck@users.noreply.github.com>
2021-04-18 20:27:38 +0300

    Add: -c flag in CLI, help message

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
+#include <iostream>
 #include <string>
 
 #include "archiver.hpp"
 
 using namespace std;
 
+void show_help();
+
 int main(int argc, char *argv[]) {
-  if (argc == 4 && argv[1] == string_view{"--compress"}) {
-    ArchiverLZW(12).compress(argv[2], argv[3]);
+  if (argc == 4) {
+    if (argv[1] == string_view{"--compress"} || argv[1] == string_view{"-c"}) {
+      ArchiverLZW(12).compress(argv[2], argv[3]);
+    } else {
+      show_help();
+    }
+  } else {
+    show_help();
   }
 
   return 0;
+}
+
+void show_help() {
+  cout << "Simple program to compress and decompress files.\n\n"
+          "Syntax: omegazip [options] input_file output_file\n"
+          "Options:\n"
+          "-h, --help           Print this Help.\n"
+          "-c, --compress       Compress file into archive.\n"
+          "-d, --decompress     Extract file from archive.\n";
 }
